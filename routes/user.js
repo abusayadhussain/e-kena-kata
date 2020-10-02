19d5efc8a204f209f61f4953026393d9ad8dbbe6
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-02 20:12:08 +0600

    added signup validation

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { signup } = require("../controllers/user");
+const { userSIgnUpValidator } = require("../validator");
 
-router.post("/signup", signup);
+router.post("/signup", userSIgnUpValidator, signup);
 
 module.exports = router;
