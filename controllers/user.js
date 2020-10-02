dc4ef4a6ed3b8150e510ea00d6b680480652228d
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-02 19:20:02 +0600

    added manual error handler

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,13 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
   console.log("request body: ", req.body);
   const user = new User(req.body);
   user.save((err, user) => {
-    if (err) return res.status(400).json({ error: err });
+    if (err) return res.status(400).json({ error: errorHandler(err) });
+    user.salt = undefined;
+    user.hashed_password = undefined;
     res.json({ user });
   });
 };
