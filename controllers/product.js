97deb5270ce57bb0663de30df9673048d102cf22
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-04 23:23:11 +0600

    added validation for products

--- controllers/product.js
+++ controllers/product.js
@@ -13,9 +13,29 @@ exports.create = (req, res) => {
         error: "Image could not be uploaded",
       });
     }
+
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
     let product = new Product(fields);
 
     if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image file should be less than 1 mb in size",
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
