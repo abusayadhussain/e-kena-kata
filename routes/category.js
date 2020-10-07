a922bcc4e14404525f1cc3faabce04c8c0fa86cc
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-07 21:52:55 +0600

    added CRUD operations on category

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const {
+  create,
+  categoryById,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/category");
 const { requireSignIn, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignIn, isAuth, isAdmin, create);
+router.put(
+  "/category/:categoryId/:userId",
+  requireSignIn,
+  isAuth,
+  isAdmin,
+  update
+);
+router.delete(
+  "/category/:categoryId/:userId",
+  requireSignIn,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.get("/categories", list);
 
 router.param("userId", userById);
+router.param("categoryId", categoryById);
 
 module.exports = router;
