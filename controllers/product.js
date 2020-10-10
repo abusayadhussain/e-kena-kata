c2f0270b81fea2550ea7797285b46223c6ae73ce
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-10 23:35:32 +0600

    implement fetching photo in the client side

--- controllers/product.js
+++ controllers/product.js
@@ -241,3 +241,11 @@ exports.listBySearch = (req, res) => {
       });
     });
 };
+
+exports.photo = (req, res) => {
+  if (req.product.photo.data) {
+    res.set("Content-Type", req.product.photo.contentType);
+    return res.send(req.product.photo.data);
+  }
+  next();
+};
--- routes/product.js
+++ routes/product.js
@@ -11,6 +11,7 @@ const {
   listRelated,
   listCategories,
   listBySearch,
+  photo,
 } = require("../controllers/product");
 const { requireSignIn, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -36,6 +37,7 @@ router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.param("userId", userById);
 router.param("productId", productById);
