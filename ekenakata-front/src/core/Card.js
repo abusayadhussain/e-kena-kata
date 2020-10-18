402d42597dff6fb8f3f3390fb4387b9a9f581eea
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-18 22:14:13 +0600

    added styling

--- ekenakata-front/src/core/Card.js
+++ ekenakata-front/src/core/Card.js
@@ -13,7 +13,7 @@ const Card = ({product}) => {
                     <p>{product.description}</p>
                     <p>{product.price}</p>
                     <Link to="/">
-                        <button className="btn btn-outline-primary mt-2 mb-2">
+                        <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
                             View Product
                         </button>
                     </Link>
