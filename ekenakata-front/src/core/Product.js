b7e8268b18320d15baf89d7fed08b53ba01dbccd
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-21 23:13:52 +0600

    show related products

--- ekenakata-front/src/core/Product.js
+++ ekenakata-front/src/core/Product.js
@@ -1,13 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
-import { read } from './apiCore';
+import { read, listRelated } from './apiCore';
 import Card from './Card';
 
 const Product = props => {
 
-    const [product,setProduct] = useState({
-
-    });
+    const [product,setProduct] = useState({});
+    const [relatedProduct,setRelatedProduct] = useState([]);
     const [error,setError] = useState(false);
 
     const loadSingleProduct = productId => {
@@ -17,6 +16,15 @@ const Product = props => {
                     setError(data.error)
                 }else{
                     setProduct(data)
+                    //fetch related product
+                    listRelated(data._id)
+                        .then(data=>{
+                            if(data.error){
+                                setError(data.error)
+                            } else{
+                                setRelatedProduct(data)
+                            }
+                        })
                 }
             })
     }
@@ -24,12 +32,22 @@ const Product = props => {
     useEffect(()=>{
         const productId = props.match.params.productId
         loadSingleProduct(productId)
-    },[])
+    },[props])
 
     return (
         <Layout title={product && product.name} description={product && product.description && product.description.substring(0,100)} className="container-fluid">
                 <div className="row">
-                {product && product.description && <Card product={product} showViewProductButton={false}/>}
+                    <div className="col-8">
+                        {product && product.description && <Card product={product} showViewProductButton={false}/>}
+                    </div>
+                    <div className="col-4">
+                        <h4>Realted Products</h4>
+                        {relatedProduct.map((p,i)=>(
+                            <div className="mb-3">
+                                <Card key={i} product={p}/>
+                            </div>
+                        ))}
+                    </div>
                 </div>
         </Layout>
     );
--- ekenakata-front/src/core/apiCore.js
+++ ekenakata-front/src/core/apiCore.js
@@ -51,3 +51,11 @@ export const read = (productId) => {
     .then(response => {return response.json()})
     .catch(err => {console.log(err)})
 };
+
+export const listRelated = (productId) => {
+    return fetch(`${API}/products/related/${productId}`, {
+        method: "GET",
+    })
+    .then(response => {return response.json()})
+    .catch(err => {console.log(err)})
+};
