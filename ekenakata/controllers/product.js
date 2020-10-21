5fe28d655f5f7d604f5369493abd917da973b3b5
Abu Sayad Hussain <abusayadhussain@topofstacksoftware.com>
2020-10-21 22:51:03 +0600

    Product detail on single product page

--- ekenakata/controllers/product.js
+++ ekenakata/controllers/product.js
@@ -5,7 +5,9 @@ const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.productById = (req, res, next, id) => {
-  Product.findById(id).exec((err, product) => {
+  Product.findById(id)
+  .populate('category')
+  .exec((err, product) => {
     if (err || !product) {
       return res.status(400).json({
         error: "Product not found!",
@@ -249,3 +251,26 @@ exports.photo = (req, res) => {
   }
   next();
 };
+
+exports.listSearch = (req, res) => {
+  // create query object to hold category and search value
+  const query = {};
+  // assign search value to query.name
+  if(req.query.search){
+    query.name = {$regex: req.query.search, $options: 'i'}
+    //assign category value to query.category
+    if(req.query.category && req.query.category != 'All'){
+      query.category = req.query.category
+    }
+    //find the product based on query object with 2 properties
+    //search & category
+    Product.find(query,(err,products)=>{
+      if(err){
+        return res.status(400).json({
+          error: errorHandler(err)
+        })
+      }
+      res.json(products)
+    }).select('-photo');
+  }
+}
--- ekenakata/routes/product.js
+++ ekenakata/routes/product.js
@@ -11,6 +11,7 @@ const {
   listRelated,
   listCategories,
   listBySearch,
+  listSearch,
   photo,
 } = require("../controllers/product");
 const { requireSignIn, isAuth, isAdmin } = require("../controllers/auth");
@@ -34,6 +35,7 @@ router.put(
 );
 
 router.get("/products", list);
+router.get("/products/search", listSearch);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
